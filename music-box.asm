0e14f74b4bfe83cb799e01c2307b1aa3e2127a7e
adeel <adeelasakkeer@gmail.com>
2023-03-02 20:23:22 +0530

    Music Box working
    Removed print and added tone play instead

--- music-box.asm
+++ music-box.asm
@@ -4,6 +4,53 @@
 .code
 ; Get a series of characters from the keyboard
 ; and store them in stack
+
+PLAY MACRO frequency               ;This macro receives the tone
+        MOV     AX,frequency       ;and sends to call the procedures
+        CALL    setFreq
+        CALL    activateSpkr
+ENDM
+
+;SEND THE FREQUENCY IN TWO SEPARATE BYTES TO THE PORT.
+setFreq  PROC                   
+        OUT     42h, AL 
+        MOV     AL, AH
+        OUT     42h, AL 
+        RET
+setFreq  ENDP
+;------------------------------------------------------------------------------------
+speakerOn  PROC                  
+        MOV AL, 182
+        OUT 43H, AL 
+        
+        IN      AL, 61h
+        OR      AL, 11B
+        OUT     61h, AL
+        RET
+speakerOn  ENDP
+
+speakerOff  PROC               
+        IN      AL, 61h
+        AND     AL, 11111100b
+        OUT     61h, AL
+        RET
+speakerOff  ENDP
+;------------------------------------------------------------------------------------
+
+activateSpkr proc                      ;Activate the horn and place the name of
+        CALL speakerOn           ;The key
+        
+        MOV     CX, 07H           ; Wait 0.5 second
+        MOV     DX, 0A120H        ;before turning off the speaker
+        MOV     AH, 86H
+        INT     15H
+
+        CALL    speakerOff       ;Horn disconnects and returns.
+        RET
+activateSpkr endp
+
+;------------------------------------------------------------------------------------
+
 keyPress PROC
         MOV     AH,08H            
         INT     21H
@@ -21,39 +68,79 @@ toLower PROC
      CONTINUE:
         RET
 toLower ENDP
+;------------------------------------------------------------------------------------
 
-; Print the stack
-PRINT PROC
-    beginPrint:
-        CMP    CX, 0 ;if the counter is 0 then there is nothing to print
+PLAYBACK PROC
+    beginPop:
+        CMP    BX, 0 ;if the counter is 0 then there is nothing to print
         JE     leavePrint 
+        DEC BX
         ADD    BP,2
         MOV    AX,[BP-2] ;get the character from the stack
-        MOV    DL, AL
-        MOV    AH, 02H 
-        INT    21H ;print the character
-        DEC    CX 
-        JMP    beginPrint ;print the next character
-    leavePrint:
-        RET
-PRINT ENDP
+        CMP     AL,'q'   ;DO high
+        JNE     S1       ;IF NOT THE EXPECTED KEY, JUMP TO CHECK NEXT.
+        PLAY    2280      ;IF IT IS THE EXPECTED KEY, IT GENERATES THE CORRESPONDING SOUNDE
+        JMP     beginPop ;AFTER THE SOUND, RESTART TO WAIT FOR ANOTHER SOUND.
+        S1:     
+            CMP     AL,'w'   ;RE high
+            JNE     S2
+            PLAY    2031
+            JMP     beginPop
+        S2:     
+            CMP     AL,'e'   ;MI high
+            JNE     S3
+            PLAY    1809
+            JMP     beginPop
+        S3:     
+            CMP     AL,'r'   ;FA high
+            JNE     S4
+            PLAY    1715
+            JMP     beginPop
+        S4:     
+            CMP     AL,'t'   ;SOL high
+            JNE     S5
+            PLAY    1521
+            JMP     beginPop
+        S5:     
+            CMP     AL,'y'   ;LA high
+            JNE     S6
+            PLAY    1355
+            JMP     beginPop
+        leavePrint:
+            RET
+        S6:     
+            CMP     AL,'u'   ;TI high
+            JNE     S7
+            PLAY    1207
+            JMP     beginPop
+        S7:     
+            CMP     AL,'i'
+            JNE     beginPop 
+            PLAY    1140
+        addCount:
+            ADD     BX, 1
+            JMP     beginPop
+PLAYBACK ENDP
 
+;------------------------------------------------------------------------------------
 
 player PROC
     push    bp
     mov     bp,sp
-    MOV     CX, 0 
+    MOV     BX, 0 
     BEGIN:
         CALL    keyPress
         CMP     AL, 27 ; ESC
         JNE     KEYED
+        MOV     sp,bp       ;restore sp
+        POP     bp 
         RET
     
     KEYED:    
         ;if the key is p then move onto printing the stack
         CMP     AL, 'p' ; 
         JNE     SAVEKEY
-        CALL    PRINT
+        CALL    PLAYBACK
         MOV     sp,bp       ;restore sp
         POP     bp 
         RET
@@ -61,7 +148,7 @@ player PROC
     SAVEKEY:
         PUSH    AX  ;save the key pressed in the stack
         SUB     BP,02
-        INC     CX  ;increment the counter
+        INC     BX  ;increment the counter
         JMP     BEGIN
 
 player ENDP
