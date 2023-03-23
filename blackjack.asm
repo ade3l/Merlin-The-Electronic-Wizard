c82ce6864fa1bb4dfaa7716d09cb3538a09d5463
adeel <adeelasakkeer@gmail.com>
2023-03-23 06:56:31 +0530

    random number codes added

--- blackjack.asm
+++ blackjack.asm
@@ -1,7 +1,12 @@
 .model small
 .stack 100h
-.data
-msg DB 'Hello World$'
+.data 
+mult    DW  25173
+incr    DW  13849          
+SEED    DW  ?
+uLimit  DB  13
+lLimit  DB  1
+msg DB 'Welcome to Blackjack$'
 .code  
 
 printStr  MACRO row, column
@@ -15,7 +20,7 @@ printStr  MACRO row, column
     MOV     BL, 02H; color
     DEC     CX       ;the calculated length includes the $ symbol. So decrement length by 1
     MOV     DH, row
-    MOV     DL,g column  
+    MOV     DL, column  
     INT     10H 
 endm      
 
@@ -26,7 +31,8 @@ start:
     PUSH    BP
     MOV     bp,sp  
     LEA     BP, msg
-    printStr   1,1 
+    printStr   1,1     
+    
     MOV     AX, 4C00H
     INT     21H 
 
@@ -35,7 +41,7 @@ strLen  PROC
     INC     CX
     MOV     AX,[SI]
     INC     SI
-    CMP     AX, '$'
+    CMP     AL, '$'
     JNE     strLen
     RET
 strLen  ENDP
@@ -56,6 +62,52 @@ toLower PROC
      CONTINUE:
         RET
 toLower ENDP
+
+;Random number generator from https://github.com/ade3l/Pseudo-random-number-generator
+genRand PROC
+    PUSH    AX
+    PUSH    BX
+    PUSH    CX
+     
+    ;calculate next value and make that the new seed
+    MOV     AX, mult
+    MOV     BX, SEED
+    MUL     BX
+    ADD     AX, incr 
+    MOV     SEED, AX
+    shr     ax, 4                                     
+    
+    ;Reducing the number to in the required range
+    MOV     BL, uLimit
+    SUB     BL, lLimit
+    
+    MOV     DX,0
+    DIV     BX
+    ADD     DL, lLimit
+    
+    POP     CX
+    POP     BX
+    POP     AX 
+    RET
+genRand ENDP 
+
+createSeed PROC
+    PUSH    AX
+    PUSH    BX
+    PUSH    CX
+    PUSH    DX 
+    
+    ;We will use Low-order part of clock count as the seed
+    MOV     AH, 00H
+    INT     1AH
+    MOV     SEED, DX
+    
+    POP     DX
+    POP     CX
+    POP     BX
+    POP     AX 
+    RET
+createSeed ENDP
     
             
         
