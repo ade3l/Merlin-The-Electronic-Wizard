df03a0724a5d9d6f8524dbbe8d838f59e6676d5d
adeel <adeelasakkeer@gmail.com>
2023-03-27 12:38:10 +0530

    Hit or stay completed

--- blackjack.asm
+++ blackjack.asm
@@ -7,17 +7,23 @@ SEED        DW  ?
 uLimit      DB  12
 lLimit      DB  1
 welcome     DB  'Welcome to Blackjack$' 
-DHandMsg    DB  'Dealers Hand$'
+DHandMsg    DB  'Dealer Hand$'
 PHandMSg    DB  'Your Hand$'
 PTotalMSg    DB  'Your Total:$' 
-
+DTotalMSg    DB  'Dealer Total:$'
 msgPbust    db "Bust! You lose!$"
 msgPwin     db "Blackjack! You win!$"
+msgAskHitOrStay db 'Do you want to hit or stay? (h/s): $'
+msgInvalidInput db 'Invalid input. Please enter "h" for hit or "s" for stay: $'
+msgPlayerHit db 'You chose to hit.$'
+msgPlayerStay db 'You chose to stay.$'
+msgPlayerWin DB 'YOU WON!$'
+msgDealerWin DB 'DELAER WON. YOU LOST!$'
 ;msgPcontinue db "Your score is less than 21. Continue playing.$", 0
-msgClear    db "                       $"    
+msgClear    db "                                    $"    
 round       DB  0
-dealerHand  db  5 dup (0)
-playerHand  db  5 dup (0)
+dealerHand  db  10 dup (0)
+playerHand  db  10 dup (0)
 dealerSize  db  0
 playerSize  db  0 
 dealerRow   db  4
@@ -27,10 +33,12 @@ playerCol   db  1
 currDCard   dW  0
 currPCard   dW  0 
 deck        db  65,50,51,52,53,54,55,56,56,74,81,75,42
-pScore db  0
+pScore db  ?
+dScore db  ?
 .code               
 
-printStr  MACRO row, column
+printStr  MACRO row, column 
+    saveRegs
     ;calculate length of the string    
     MOV     CX, 00      ;length will be stroed in cx
     MOV     SI, BP
@@ -42,7 +50,8 @@ printStr  MACRO row, column
     DEC     CX       ;the calculated length includes the $ symbol. So decrement length by 1
     MOV     DH, row
     MOV     DL, column  
-    INT     10H 
+    INT     10H
+    restoreRegs 
 endm  
 
 saveRegs    MACRO
@@ -112,7 +121,7 @@ start:
     CALL    dispDHand 
     CALL    dispPHand
     CALL    checkP21
-    
+    CALL    askHitOrStay
     MOV     AX, 4C00H
     INT     21H 
 ;---------------------------------------------------------------------------------------
@@ -205,22 +214,28 @@ dispPHand   ENDP
 
 checkP21     PROC
    saveRegs
+   LEA      BP, msgClear
+   printstr 18,40
+   printstr 19,40
    MOV      BX, 00  ;TOTAL VALUE
    MOV      CX, 00  ;TOTAL NUMBER OF ACES
-   XOR      AX, AX
    LEA      DX, playerHand
    MOV      SI, DX
    ch1:
     MOV     AX, [SI]
+    XOR      AH, AH
     INC     SI
     CMP     AX, 0   
     JE      calcP
     CMP     AX, 1   ;check if ace. Value of ACE's will be calculated at the end
     JE      ACE1
-    
+    CMP     AX, 10  ;check if face card
+    JAE     FACE1   ;add 10 if it is
     ADD     BL, AL  ;add value of non-ace card to the total
     JMP     ch1
-   
+   FACE1:
+    ADD     BL, 10
+    JMP     CH1
    ACE1:
     INC     CL
     JMP     ch1
@@ -312,6 +327,195 @@ checkPScore PROC
     RET
 checkPScore ENDP
 
+askHitOrStay PROC
+    saveRegs
+    ; Display message to ask player to hit or stay
+    LEA BP, msgAskHitOrStay
+    printstr 18, 40
+    
+    ; Wait for player input (h for hit, s for stay)
+    CALL    KEYPRESS
+    
+    ; Check if player chose to hit or stay
+    cmp al, 'h'
+    je hit
+    cmp al, 's'
+    je stay
+    
+    ; Invalid input, ask again
+    LEA BP, msgInvalidInput
+    printstr 18, 40
+    JMP askHitOrStay
+    
+    hit:
+        ; Player chose to hit
+        LEA BP, msgPlayerHit
+        printstr 19, 40
+        JMP hitP
+        restoreRegs
+        RET
+        
+    stay:
+        ; Player chose to stay
+        LEA BP, msgPlayerStay
+        printstr 19, 40
+        JMP stayP
+        restoreRegs
+        RET
+        
+askHitOrStay ENDP
+hitP     PROC
+    CALL    dealPlayer
+    CALL    dispPHand
+    CALL    checkP21
+    CALL    askHitOrStay
+    RET
+hitP     ENDP 
+
+STAYP    PROC 
+    DEC dealerCol
+    setCursor dealerRow, dealerCol 
+    INC dealerCol
+    MOV BL, 1
+    LEA DX, dealerHand
+    ADD DX, 1
+    MOV SI, DX
+    MOV AL, [SI]
+    dispCard AL
+    
+    
+    
+    LEA      BP, msgClear
+    printstr 18,40
+    printstr 19,40 
+    LEA     BP, DTotalMsg
+    PRINTSTR    15, 15
+    d17Check:
+        CALL    CalcDTotal
+        CALL    showDScore
+        
+        ; Check if dealer's total is less than 17
+        CMP dScore, 17
+        JL  DEALP
+        
+        ; Check if dealer has bust
+        CMP dScore, 22
+        JAE  PLAYER_WIN
+        
+        MOV AH, dScore
+        MOV AL, pScore
+        ; Check if dealer has higher hand than player
+        CMP AH, AL
+        JG  DEALER_WIN
+        
+        ; If none of the above conditions are true, player wins
+        JMP PLAYER_WIN        
+
+
+    DEALP:
+        CALL dealDealer
+        CALL dispDHand
+        JMP  d17Check
+    
+    
+    PLAYER_WIN:
+        LEA BP, msgPlayerWin
+        PRINTSTR 18, 40
+        JMP END_GAME 
+        
+    DEALER_WIN:
+        LEA BP, msgDealerWin
+        PRINTSTR 18, 40
+        JMP END_GAME
+        
+    END_GAME:
+        MOV AX, 4C00H
+        INT 21H
+STAYP    ENDP
+
+CalcDTotal     PROC
+   saveRegs
+   MOV      BX, 0  ;TOTAL VALUE
+   MOV      CX, 0  ;TOTAL NUMBER OF ACES
+   LEA      DX, dealerHand
+   MOV      SI, DX
+   ch3:
+    MOV     AX, [SI]
+    XOR     AH, AH
+    INC     SI
+    CMP     AX, 0   
+    JE      calcD
+    CMP     AX, 1   ;check if ace. Value of ACE's will be calculated at the end
+    JE      ACE2
+    CMP     AX, 10  ;check if face card
+    JAE     FACE2   ;add 10 if it is
+    ADD     BL, AL  ;add value of non-ace card to the total
+    JMP     ch3
+   FACE2:
+    ADD     BL, 10
+    JMP     CH3
+   ACE2:
+    INC     CX
+    JMP     ch3
+    
+   addAce2:
+    ADD     BL, 11
+    DEC     CX
+     
+   calcD:
+    CMP     CX, 0   ;check if there are any aces. if not then leave
+    JE      exChd21
+    CMP     BL, 10  ;check if current total is <= 10
+    JLE     addAce2  ;if it is then add 11 for that ACE's values
+    ch4: 
+        ADD     BL, 1 ;otherwise add 1 for it's value
+        LOOP    ch4    
+    
+   exChd21:
+    MOV     dScore, BL
+    
+    ;CALL    checkDScore    
+    restoreRegs
+    RET
+CalcDTotal     ENDP
+
+showDScore  PROC
+    saveRegs
+    XOR     CX, CX
+     
+    MOV AL, dScore
+    ; Convert the hex value to decimal and store the digits in the stack
+    MOV AH, 0         ; Initialize the quotient to 0
+    MOV CX, 10        ; Initialize the divisor to 10 
+    DIV_LOOP2:
+        XOR DX, DX        ; Clear the DX register
+        DIV CX            ; Divide the quotient by the divisor
+        ADD DL, '0'       ; Convert the remainder to ASCII
+        PUSH DX           ; Save the digit on the stack
+        CMP AL, 0         ; Check if the quotient is 0
+        JNE DIV_LOOP2     ; If not, continue dividing
+    CMP dScore, 9
+    JA  PRINT2
+    MOV AX, 30H
+    PUSH    AX
+    PRINT2:
+        setCursor   15, 28
+        ; Pop each digit from the stack and print it to the screen
+        POP AX
+        MOV AH, 09H ; Set up the function code for printing a character
+        MOV CX, 1
+        MOV BL, 2
+        INT 10H     ; Call the BIOS to print the character
+        setCursor   15, 29
+        ; Pop each digit from the stack and print it to the screen
+        POP AX
+        MOV AH, 09H ; Set up the function code for printing a character
+        MOV CX, 1
+        MOV BL, 2
+        INT 10H     ; Call the BIOS to print the character
+    restoreRegs
+    RET  
+showDScore ENDP
 
 
 endGame     PROC
