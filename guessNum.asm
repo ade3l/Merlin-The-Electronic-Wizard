ad3795d50b94bc4bb3006a59f745b34e8a271463
adeel <adeelasakkeer@gmail.com>
2023-04-05 12:33:23 +0530

    show selected level and range

--- guessNum.asm
+++ guessNum.asm
@@ -8,7 +8,16 @@ uLimit  DW  ?
 lLimit  DW  1   
 diff_msg dw "Select your difficulty level 1-3:" 
 diff_msg_len equ $-diff_msg
+selected_msg db "Selected level: "
+selected_msg_len equ    $-selected_msg
+l1_msg db "Mystery number is between 10 - 99" 
+l1_msg_len dw 33
+l2_msg db "Mystery number is between 100 - 999"
+l2_msg_len dw 35
+l3_msg db "Mystery number is between 1000 - 9999" 
+l3_msg_len dw 37 
 diff db ?
+secret dw ?
 .code 
 saveRegs    MACRO
     PUSH    AX
@@ -38,18 +47,47 @@ printStr  MACRO row, column, length
     INT     10H
     restoreRegs 
 endm  
+CLRSCR MACRO
+    ; Macro to clear the screen
+    mov ax, 3
+    int 10h
 
+ENDM
+setCursor   MACRO   row, column
+    saveRegs
+    MOV     AH, 02H
+    MOV     DH, row 
+    MOV     DL, column
+    INT     10H
+    restoreRegs
+endm 
 start:
     MOV     AX,@data
     MOV     DS,AX 
     MOV     ES,AX
+
     PUSH    BP
     MOV     BP,SP
     
     CALL    createSeed
     MOV BP,OFFSET diff_msg 
-    printStr    10, 10, diff_msg_len
-    call getDiff 
+    printStr    1, 1, diff_msg_len
+    call getDiff
+    CLRSCR 
+    ;Show selected level---
+    MOV BP,OFFSET selected_msg 
+    printStr    1, 1, selected_msg_len
+    setCursor   1, selected_msg_len+1 
+    MOV AH, 09H
+    MOV AL, diff
+    ADD  AL, 30H
+    MOV BL, 02H
+    MOV CX, 1
+    INT 10H 
+    
+    ;Show range 
+    call showRange
+    call createSecret
     MOV     AX, 4C00H
     INT     21H 
 
@@ -68,6 +106,47 @@ getDiff proc
     RET
 getDiff ENDP
 
+createSecret    proc
+    cmp diff, 1
+    je  createSecret1,
+    cmp diff, 2
+    je  createSecret2
+    MOV lLimit, 1000
+    MOV uLimit, 9999 
+    jmp getRand
+    createSecret1:
+        MOV lLimit, 10
+        MOV uLimit, 99 
+        jmp getRand
+        
+    createSecret2:
+        MOV lLimit, 100
+        MOV uLimit, 999
+    getRand:
+        CALL genRand
+        mov secret, DX
+    ret
+createSecret endp 
+
+showRange   proc 
+    cmp diff, 1
+    je  range1 
+    cmp diff, 2
+    je  range2
+    
+    MOV BP,OFFSET l3_msg 
+    printStr    2, 1, l3_msg_len
+    ret
+    
+    range1:
+        MOV BP,OFFSET l1_msg 
+        printStr    2, 1, l1_msg_len
+        ret 
+    range2:
+        MOV BP,OFFSET l2_msg 
+        printStr    2, 1, l2_msg_len
+        ret         
+    
 genRand PROC 
     
     PUSH    AX
@@ -93,7 +172,6 @@ genRand PROC
     POP     CX
     POP     BX
     POP     AX
-    INT 3h 
     RET
 genRand ENDP 
 
